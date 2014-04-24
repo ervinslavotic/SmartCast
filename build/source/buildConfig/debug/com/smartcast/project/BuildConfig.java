a022dbbc0db6e2a0a24fcfb9827cf493a554620e
Ervin Slavotic <ervin.slavotic2@mail.dcu.ie>
2014-04-24 03:12:57 +0100

    commit

---
+++ build/source/buildConfig/debug/com/smartcast/project/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.smartcast.project;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String PACKAGE_NAME = "com.smartcast.project";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+}
